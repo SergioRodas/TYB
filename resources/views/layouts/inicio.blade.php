99ab27d44c2154020e97c9463eff6614e266da6b
sergiorodas <sergiorodasbj@hotmail.com>
2020-04-17 08:17:23 -0300

    Agrega dos imágenes al carrusel, próximo commit mejorará la imagen restante

--- resources/views/layouts/inicio.blade.php
+++ resources/views/layouts/inicio.blade.php
@@ -78,10 +78,10 @@
                       <img class="d-block w-100" src="img/portada.jpg" alt="First slide">
                     </div>
                     <div class="carousel-item">
-                      <img class="d-block w-100" src="img/portada.jpg" alt="Second slide">
+                      <img class="d-block w-100" src="img/bg-108.jpg" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                      <img class="d-block w-100" src="img/portada.jpg" alt="Third slide">
+                      <img class="d-block w-100" src="img/bg-109.jpg" alt="Third slide">
                     </div>
                   </div>
                   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -164,13 +164,13 @@
                   </ol> --}}
                   <div class="carousel-inner">
                     <div class="carousel-item active">
-                      <img class="d-block w-100" src="img/portada.jpg" alt="First slide">
+                      <img class="d-block w-100" src="img/bg-108.jpg" alt="First slide">
                     </div>
                     <div class="carousel-item">
                       <img class="d-block w-100" src="img/portada.jpg" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                      <img class="d-block w-100" src="img/portada.jpg" alt="Third slide">
+                      <img class="d-block w-100" src="img/bg-109.jpg" alt="Third slide">
                     </div>
                   </div>
                   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
